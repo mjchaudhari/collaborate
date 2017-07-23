7d55c7e8ed41cca85be43f91284446eea048b60d
Mahesh Chaudhari <mjchaudhari@Maheshs-MacBook-Pro.local>
2017-07-23 10:31:58 +0100

    Fixed the verticle ceter styles

--- serverConfig.js
+++ serverConfig.js
@@ -4,7 +4,7 @@
 module.exports = {
     //mongoURI : 'mongodb://127.0.0.1:27017/easyapp'
     mongoURI : 'mongodb://admin:admin@ds050077.mongolab.com:50077/easyapp'
-    , webRootDir : '/dist' //"/dist"   //'/www'
+    , webRootDir : '/www' //"/dist"   //'/www'
     , tempFileStore : "./tmpStore"
 
 }
